39c5dd53d9067a50cfaaf98d82fa618cbc3541bf
Stephanie Marzan <s.marzanoberholz@ufl.edu>
2023-05-27 17:52:08 -0500

    register page base html

---
+++ pages/register.js
@@ -0,0 +1,10 @@
+export default function Register (){
+    return(
+        <div>
+            <h1>Register</h1>
+            <input type="text" placeholder="username" name="username"></input>
+            <input type="text" placeholder="password" name="password"></input>
+            <button>Register</button>
+        </div>
+    )
+}
